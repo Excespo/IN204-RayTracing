5baaa96524276fa1affe6ff51f53bf7e1f2473b0
legion <excespo@qq.com>
2024-03-18 21:23:58 +0100

    Debug camera

--- camera.h
+++ camera.h
@@ -39,7 +39,7 @@ public:
                     Ray ray = get_ray(i, j);
                     pixel_color += ray_color(ray, max_depth, world);
                 }
-                write_color(std::cout, 1/samples_per_pixel * pixel_color);
+                write_color(std::cout, pixel_samples_scale * pixel_color);
             }
         }
 
@@ -48,6 +48,7 @@ public:
 
 private:
     int    image_height;    // Rendered image height
+    double pixel_samples_scale;
     Point3d center;          // Camera center
     Point3d pixel_00_loc;     // Location of pixel 0, 0
     Vector3d   pixel_delta_u;   // Offset to pixel to the right
@@ -57,16 +58,16 @@ private:
     Vector3d   defocus_disk_v;  // Defocus disk vertical radius
 
     void initialize() {
-        image_height = static_cast<int>(image_width / aspect_ratio);
+        image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
-
+        pixel_samples_scale = 1. / samples_per_pixel;
         center = look_from;
 
         // Determine viewport dimensions.
         auto theta = degrees_to_radians(vertical_fov);
         auto h = tan(theta/2);
         auto viewport_height = 2 * h * focus_dist;
-        auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+        auto viewport_width = viewport_height * (double(image_width)/image_height);
 
         // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
         w = unit_vector(look_from - look_at);
@@ -83,7 +84,7 @@ private:
 
         // Calculate the location of the upper left pixel.
         auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
-        pixel_00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+        pixel_00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v)/2;
 
         // Calculate the camera defocus disk basis vectors.
         auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
@@ -92,13 +93,23 @@ private:
     }
 
     Ray get_ray(int i, int j) const {
-        // Get a randomly-sampled camera ray for the pixel at location i,j, originating from
-        // the camera defocus disk.
-
-        auto pixel_center = pixel_00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-        auto pixel_sample = pixel_center + pixel_sample_square();
-
-        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
+//        // Get a randomly-sampled camera ray for the pixel at location i,j, originating from
+//        // the camera defocus disk.
+//
+//        auto pixel_center = pixel_00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+//        auto pixel_sample = pixel_center + pixel_sample_square();
+//
+//        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
+//        auto ray_direction = pixel_sample - ray_origin;
+//
+//        return Ray(ray_origin, ray_direction);
+        // Construct a camera ray originating from the defocus disk and directed at a randomly
+        // sampled point around the pixel location i, j
+        auto offset = pixel_sample_square();
+        auto pixel_sample = pixel_00_loc +
+                            + ((i + offset.get_x()) * pixel_delta_u)
+                            + ((j + offset.get_y()) * pixel_delta_v);
+        auto ray_origin = (defocus_angle <= 0) ? center: defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         return Ray(ray_origin, ray_direction);
@@ -106,15 +117,10 @@ private:
 
     Vector3d pixel_sample_square() const {
         // Returns a random point in the square surrounding a pixel at the origin.
-        auto px = -0.5 + random_double();
-        auto py = -0.5 + random_double();
-        return (px * pixel_delta_u) + (py * pixel_delta_v);
-    }
-
-    Vector3d pixel_sample_disk(double radius) const {
-        // Generate a sample from the disk of given radius around a pixel at the origin.
-        auto p = radius * random_in_unit_disk();
-        return (p[0] * pixel_delta_u) + (p[1] * pixel_delta_v);
+//        auto px = -0.5 + random_double();
+//        auto py = -0.5 + random_double();
+//        return (px * pixel_delta_u) + (py * pixel_delta_v);
+        return Vector3d(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
     Point3d defocus_disk_sample() const {
@@ -123,22 +129,22 @@ private:
         return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
-    Color ray_color(const Ray& r, int depth, const Hittable& world) const {
+    Color ray_color(const Ray& ray, int depth, const Hittable& hittable_obj) const {
         // If we've exceeded the ray bounce limit, no more light is gathered.
         if (depth <= 0)
             return Color(0,0,0);
 
         HitStatus stat;
 
-        if (world.hit(r, Interval(0.001, inf), stat)) {
+        if (hittable_obj.hit(ray, Interval(0.001, inf), stat)) {
             Ray scattered;
             Color attenuation;
-            if (stat.material->scatter(r, stat, attenuation, scattered))
-                return attenuation * ray_color(scattered, depth-1, world);
+            if (stat.material->scatter(ray, stat, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth-1, hittable_obj);
             return Color(0,0,0);
         }
 
-        Vector3d unit_direction = unit_vector(r.direction());
+        Vector3d unit_direction = unit_vector(ray.direction());
         auto a = 0.5*(unit_direction.get_y() + 1.0);
         return (1.0-a)*Color(1.0, 1.0, 1.0) + a*Color(0.5, 0.7, 1.0);
     }
