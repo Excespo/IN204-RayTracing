a3d5cfe98f92e76311ea774abf9cb266ced4183f
Excespo's MacbookAir <excespo@qq.com>
2024-03-20 17:04:49 +0100

    add progress bar

--- utils.h
+++ utils.h
@@ -32,5 +32,38 @@ inline double random_double(double min, double max) {
     return min + (max - min) * random_double();
 }
 
+class ProgressBar {
+public:
+    explicit ProgressBar(int total_steps) : total_steps(total_steps), start_time(std::chrono::high_resolution_clock::now()) {}
+
+    void update(int current_step) {
+        double progress = double(current_step) / total_steps;
+        int pos = int(progressBarWidth * progress);
+
+        auto current_time = std::chrono::high_resolution_clock::now();
+        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count();
+        double total_time_est = elapsed_seconds / progress;
+        int remaining_time = int((1.0 - progress) * total_time_est);
+
+        std::clog << "\r[";
+        for (int k = 0; k < progressBarWidth; ++k) {
+            if (k < pos) std::clog << "=";
+            else if (k == pos) std::clog << ">";
+            else std::clog << " ";
+        }
+        std::clog << "] " << int(progress * 100.0) << "% ";
+        std::clog << "Time Remaining: " << remaining_time / 60 << "m " << remaining_time % 60 << "s ";
+        std::clog << std::flush;
+    }
+
+    void done() {
+        std::clog << std::endl << "Done.\n";
+    }
+
+private:
+    int total_steps;
+    const int progressBarWidth = 50;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+};
 
 #endif //RAY_TRACING_UTILS_H
