0ec002725afad659d9c17ebd7558d5fb78fed7c8
Excespo's MacbookAir <excespo@qq.com>
2024-03-20 17:05:33 +0100

    add function to script

--- run.sh
+++ run.sh
@@ -4,4 +4,13 @@ cmake -B build
 cmake --build build
 
 echo -e "\nPlaying ray tracing ..."
-build/ray_tracing > image.ppm
+mkdir -p result
+MESSAGE_TO="result/log.txt"
+touch ${MESSAGE_TO}
+
+output_file="result/image.ppm"
+MESSAGE="30 samples p+a"
+N_SAMPLES=30
+MAX_DEPTH=10
+
+build/ray_tracing ${output_file} -p -a -m ${MESSAGE} -s ${N_SAMPLES} -d ${MAX_DEPTH}
