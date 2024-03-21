7de12a6d895a60882c2072479a7e6d7eb938205b
williamjizihe <william_ji@yeah.net>
2024-03-21 11:53:35 +0100

    load scene from json

--- run.bat
+++ run.bat
@@ -18,13 +18,13 @@ set image_width=400
 set n_threads=12
 
 set output_name=result\image_%image_width%_%n_samples%
-set extension=ppm
+set extension=png
 set output_file=%output_name%.%extension%
 :: set output_file=cout
 
 set MESSAGE=Ray tracing with %n_samples% samples, %max_depth% max depth, %image_width% image width, %n_threads% threads
 
 :: Run the program
-build\ray_tracing %output_file% -m "%MESSAGE%" -s %n_samples% -d %max_depth% -w %image_width% -a -p -n %n_threads%
+build\ray_tracing %output_file% -m "%MESSAGE%" -s %n_samples% -d %max_depth% -w %image_width% -a -p -n %n_threads% -f scene.json
 
 endlocal
