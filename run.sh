4c1555a4698acfeffde843465c83e2ca388ae5e1
williamjizihe <william_ji@yeah.net>
2024-03-21 08:14:30 +0100

    Update message

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ extension="png"
 output_file="${output_name}.${extension}"
 #output_file="cout"
 
-MESSAGE="10samples on 4 threads, 1280, with quad on spheres"
+MESSAGE="n_samples=${n_samples}, max_depth=${max_depth}, image_width=${image_width}, n_threads=${n_threads}"
 
 build/ray_tracing ${output_file} -m "${MESSAGE}" \
                   -s ${n_samples} -d ${max_depth} -w ${image_width} -a \
